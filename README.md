1b0062e44d35d23d83ded965e20a2fedff5c2d25
ToKc <ant.tokc@gmail.com>
2022-01-19 12:57:00 +0200

    finish lesson 1.2 (Hello World)

---
+++ README.md
@@ -0,0 +1,3 @@
+# devstack_intern_lesson1
+
+Стажерская программа DEV-STACK FLUTTER. Урок 1
---
+++ android/app/src/main/kotlin/com/example/devstack_intern_lesson1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.devstack_intern_lesson1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
